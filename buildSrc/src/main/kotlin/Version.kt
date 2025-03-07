93fa1db4bb415df61c74a6b2333fa66f752db7d0
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-03-07 15:40:47 +0300

    init gradle

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,8 @@
+object Version {
+    const val jacoco = "0.8.12"
+    const val jmh = "1.37"
+    const val jupiter = "5.11.4"
+    const val jvmTarget = "17"
+    const val kotlin = "1.9.24"
+    const val ktlint = "0.50.0"
+}
