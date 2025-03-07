54d09784fb0eb1d171c0ddfd99c8de3266193181
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-03-07 17:17:24 +0300

    matrix ops

--- lib/src/main/kotlin/sp/kx/math/Vertex.kt
+++ lib/src/main/kotlin/sp/kx/math/Vertex.kt
@@ -5,3 +5,11 @@ interface Vertex {
     val y: Double
     val z: Double
 }
+
+fun Vertex.times(matrix: Matrix): Vertex {
+    return MutableVertex(
+        x = matrix.m00 * x + matrix.m01 * y + matrix.m02 * z + matrix.m03,
+        y = matrix.m10 * x + matrix.m11 * y + matrix.m12 * z + matrix.m13,
+        z = matrix.m20 * x + matrix.m21 * y + matrix.m22 * z + matrix.m23,
+    )
+}
