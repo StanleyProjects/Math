ba2c5434918345c204e05d98c5659714e69effa0
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-03-09 15:13:06 +0300

    rotation

---
+++ lib/src/main/kotlin/sp/kx/math/MutableRotation.kt
@@ -0,0 +1,7 @@
+package sp.kx.math
+
+class MutableRotation(
+    override var aX: Double,
+    override var aY: Double,
+    override var aZ: Double,
+) : Rotation
---
+++ lib/src/main/kotlin/sp/kx/math/Rotation.kt
@@ -0,0 +1,7 @@
+package sp.kx.math
+
+interface Rotation {
+    val aX: Double
+    val aY: Double
+    val aZ: Double
+}
